9e34d5d4b3d5422270b4de469761f394306ed4b8
Amirwsen <a.h.aghazadeh.1381@gmail.com>
2024-09-24 16:33:13 +0330

    Fix Some Bugs

--- Domain/Requests/InsuranceOrderRequest.cs
+++ Domain/Requests/InsuranceOrderRequest.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Domain.Enums;
 
 namespace infrastructure.Repositories;
@@ -5,6 +6,8 @@ namespace infrastructure.Repositories;
 public class InsuranceOrderRequest
 {
     public Guid InsuranceId { get; set; }
+    [Required]
     public InsuranceType Type { get; set; }
+    [Required, Range(2000, 50000000)]
     public double Investment { get; set; }
 }
